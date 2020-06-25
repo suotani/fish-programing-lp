3e86a9e5f4215bc395f5aa65c31206f12175ad92
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-06-25 16:19:48 +0900

    add modal inquiry

--- script.js
+++ script.js
@@ -14,5 +14,38 @@ $(function(){
 
   $("#toTop").on("click", function(){
     $("body, html").animate({ scrollTop: 0}, 2000);
-  })
+  });
+
+  $("#inquirySubmit").on("click", function(){
+    $("#inquiryLoading").show();
+    if($("#exampleInputEmail1").val() === ""){
+      $("#inquiryLoading").hide();
+      $("#inquiryResult").text("メールアドレスを入力して下さい");
+      $("#inquiryResult").show();
+    }else{
+      $.ajax({
+        type : 'POST',
+        url : 'https://aquarium-fish-programing.herokuapp.com/inquiry',
+        dataType : 'application/json',
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*"
+        },
+        xhrFields: {
+          withCredentials: true
+        },
+        success : function(data, status, xhr){
+          let message = data["message"];
+          $("#inquiryLoading").hide();
+          $("#inquiryResult").text(message);
+          $("#inquiryResult").show();
+        },
+        error : function(){
+          $("#inquiryLoading").hide();
+          $("#inquiryResult").text("失敗しました");
+          $("#inquiryResult").show();
+        }
+      })
+    }
+  });
 });
